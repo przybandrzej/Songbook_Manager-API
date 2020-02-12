0c1a04967aca26a184164ced34d9a2a3345dca16
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-02-12 23:19:48 +0100

    generated Spring project

---
+++ src/main/java/com/lazydev/stksongbook/webapp/StkSongbookApplication.java
@@ -0,0 +1,13 @@
+package com.lazydev.stksongbook.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StkSongbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StkSongbookApplication.class, args);
+	}
+
+}
