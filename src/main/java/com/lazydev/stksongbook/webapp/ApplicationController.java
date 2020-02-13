9cb9f0712be44e9cf266647b29063eb8d68e3e80
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-02-13 03:12:03 +0100

    added JSP files and ThymeLeaf support

---
+++ src/main/java/com/lazydev/stksongbook/webapp/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.lazydev.stksongbook.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApplicationController {
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/contact.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix:"c" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact</title>
+</head>
+
+<body>
+    <h1>Copyright LazyDev</h1>
+</body>
+</html>
