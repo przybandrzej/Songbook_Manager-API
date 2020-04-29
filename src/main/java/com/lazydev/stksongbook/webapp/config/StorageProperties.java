86eae408ba2289f29acae52f5c4cd5dbe7a1da24
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-04-29 14:06:36 +0200

    added basic service layer

---
+++ src/main/java/com/lazydev/stksongbook/webapp/config/StorageProperties.java
@@ -0,0 +1,17 @@
+package com.lazydev.stksongbook.webapp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "storage")
+public class StorageProperties {
+
+  private String location;
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+}
