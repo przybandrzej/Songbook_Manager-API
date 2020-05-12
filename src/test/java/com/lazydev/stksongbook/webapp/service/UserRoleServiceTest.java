fbbbf7aa3583c66590235d56ad7b671dfba40260
Andrzej <andrzej.przybysz01@gmail.com>
2020-05-12 20:29:28 +0200

    add tests for User Role and User services

--- src/test/java/com/lazydev/stksongbook/webapp/service/UserRoleServiceTest.java
+++ src/test/java/com/lazydev/stksongbook/webapp/service/UserRoleServiceTest.java
@@ -1,8 +1,8 @@
 package com.lazydev.stksongbook.webapp.service;
 
-import com.lazydev.stksongbook.webapp.data.model.Category;
-import com.lazydev.stksongbook.webapp.data.model.Song;
-import com.lazydev.stksongbook.webapp.repository.CategoryRepository;
+import com.lazydev.stksongbook.webapp.data.model.User;
+import com.lazydev.stksongbook.webapp.data.model.UserRole;
+import com.lazydev.stksongbook.webapp.repository.UserRoleRepository;
 import com.lazydev.stksongbook.webapp.service.exception.CannotDeleteEntityException;
 import com.lazydev.stksongbook.webapp.service.exception.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
@@ -19,54 +19,54 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-class CategoryServiceTest {
+class UserRoleServiceTest {
   /**
    * Tests for all methods might not make sense since most of them just call the JpaRepository.
    * The following tested methods are the only that required tests.
    */
 
   @Mock
-  CategoryRepository repository;
+  UserRoleRepository repository;
 
   @InjectMocks
-  CategoryService service = new CategoryService(repository);
+  UserRoleService service = new UserRoleService(repository);
 
   @Test
   void findById() {
-    Category cat = new Category(1L, "category sample", new HashSet<>());
+    UserRole userRole = new UserRole(1L, "userRole sample", new HashSet<>());
     Mockito.when(repository.findById(2L)).thenReturn(Optional.empty());
-    Mockito.when(repository.findById(1L)).thenReturn(Optional.of(cat));
+    Mockito.when(repository.findById(1L)).thenReturn(Optional.of(userRole));
 
     assertDoesNotThrow(() -> service.findById(1L));
     assertNotNull(service.findById(1L));
-    assertEquals(cat, service.findById(1L));
+    assertEquals(userRole, service.findById(1L));
 
     assertThrows(EntityNotFoundException.class, () -> service.findById(2L));
   }
 
   @Test
   void findByName() {
-    Category found = new Category(1L, "category sample", new HashSet<>());
+    UserRole found = new UserRole(1L, "userRole sample", new HashSet<>());
     Mockito.when(repository.findByName("John")).thenReturn(Optional.empty());
-    Mockito.when(repository.findByName("category sample")).thenReturn(Optional.of(found));
+    Mockito.when(repository.findByName("userRole sample")).thenReturn(Optional.of(found));
 
-    assertDoesNotThrow(() -> service.findByName("category sample"));
-    assertNotNull(service.findByName("category sample"));
-    assertEquals(found, service.findByName("category sample"));
+    assertDoesNotThrow(() -> service.findByName("userRole sample"));
+    assertNotNull(service.findByName("userRole sample"));
+    assertEquals(found, service.findByName("userRole sample"));
 
     assertThrows(EntityNotFoundException.class, () -> service.findByName("John"));
   }
 
   @Test
   void deleteById() {
-    Song song = new Song();
-    song.setId(1L);
-    Category categoryWithSong = new Category(1L, "category", Set.of(song));
-    Category emptyCategory = new Category(3L, "empty cat", new HashSet<>());
+    User user = new User();
+    user.setId(1L);
+    UserRole userRoleWithUser = new UserRole(1L, "userRole", Set.of(user));
+    UserRole emptyUserRole = new UserRole(3L, "empty role", new HashSet<>());
 
-    Mockito.when(repository.findById(1L)).thenReturn(Optional.of(categoryWithSong));
+    Mockito.when(repository.findById(1L)).thenReturn(Optional.of(userRoleWithUser));
     Mockito.when(repository.findById(2L)).thenReturn(Optional.empty());
-    Mockito.when(repository.findById(3L)).thenReturn(Optional.of(emptyCategory));
+    Mockito.when(repository.findById(3L)).thenReturn(Optional.of(emptyUserRole));
 
     assertThrows(CannotDeleteEntityException.class, () -> service.deleteById(1L));
     assertThrows(EntityNotFoundException.class, () -> service.deleteById(2L));
