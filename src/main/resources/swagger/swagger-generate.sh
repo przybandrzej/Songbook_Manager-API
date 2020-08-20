922460dcd8223acc228f8ab759fdaa0f2d160d05
Andrzej <andrzej.przybysz01@gmail.com>
2020-08-20 21:15:01 +0200

    add swagger codegen cli

---
+++ src/main/resources/swagger/swagger-generate.sh
@@ -0,0 +1,11 @@
+java -jar swagger-codegen-cli.jar generate \
+  -i http://localhost:3000/v2/api-docs \
+  --api-package com.lazydev.stksongbook.mailer.client.api \
+  --model-package com.lazydev.stksongbook.mailer.client.model \
+  --invoker-package com.lazydev.stksongbook.mailer.client.invoker \
+  --group-id com.lazydev \
+  --artifact-id mailer.client \
+  --artifact-version 0.0.1-SNAPSHOT \
+  -l java \
+  --library resttemplate \
+  -o ../../../../../
