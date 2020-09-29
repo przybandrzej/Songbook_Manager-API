89269186b7f466483301f779a7a3e56ff035bd61
Andrzej <andrzej.przybysz01@gmail.com>
2020-09-29 06:44:15 +0200

    fix regex polish characters and add executable script

--- src/main/java/com/lazydev/stksongbook/webapp/util/Constants.java
+++ src/main/java/com/lazydev/stksongbook/webapp/util/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
 
   public static final String DATE_TIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
 
-  public static final String NAME_REGEX_SHORT = "^[a-zA-Z 0-9]{2,20}$";
-  public static final String USERNAME_REGEX = "^[a-zA-Z0-9 _-]{4,15}$";
+  public static final String NAME_REGEX_SHORT = "^[a-zA-Z 0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,20}$";
+  public static final String USERNAME_REGEX = "^[a-zA-Z0-9 _-żźćńółęąśŻŹĆĄŚĘŁÓŃ]{4,15}$";
   public static final String EMAIL_REGEX = "^(([^\\<\\>\\(\\)\\[\\]\\.,;:\\s@\"]+(.[^\\<\\>\\(\\)\\[\\]\\.,;:\\s@\"]+)*)|(\".+\"))@(([^\\<\\>\\(\\)\\[\\]\\.,;:\\s@\"]+.)+[^\\<\\>\\(\\)\\[\\]\\.,;:\\s@\"]{2,})$";
   public static final String PASSWORD_REGEX = "^(.){6,128}$";
-  public static final String NAME_REGEX = "^[a-z A-Z]{2,15}";
+  public static final String NAME_REGEX = "^[a-z A-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,15}";
 
   public static final String NAME_SHORT_INVALID_MESSAGE = "The name must consist of alphanumeric characters and be 2-20 characters long.";
   public static final String TITLE_INVALID_MESSAGE = "The title must consist of alphanumeric characters and be 2-20 characters long.";
